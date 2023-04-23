99c61c2be83f982f24bfdb49d06e732f90998476
Nadezhda Kotelnikova <n.kotelnikova@cs.money>
2023-04-23 23:23:56 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :response-time ms :body'))
+app.use(cors())
+app.use(express.json())
+
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+    const newId = Math.floor(Math.random() * 1000000000);
+    person = persons.find(p => p.id === newId)
+    while (person) {
+        const newId = Math.floor(Math.random() * 1000000000);
+        person = persons.find(p => p.id === newId)
+    }
+    return newId
+}
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const html_response = `<div> Phonebook has info ${persons.length} people <p> ${new Date()}</p></div>`
+    response.send(html_response)
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'Name or number missing'
+        })
+    }
+
+    const name = persons.find(p => {
+        console.log(p)
+        console.log(body)
+       return p.name.toLowerCase() === body.name.toLowerCase()
+    })
+
+    if (name) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+    persons = persons.concat(person)
+
+    return response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
