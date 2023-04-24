6db87d38b8e8c78d9b234f0493d305d7d89c5939
Nadezhda Kotelnikova <n.kotelnikova@cs.money>
2023-04-25 01:14:01 +0300

    added build folder

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms :body'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 
 let persons = [
