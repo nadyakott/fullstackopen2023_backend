120aa1d9770251fee1afe223ad5f58a450ed7f82
Nadezhda Kotelnikova <n.kotelnikova@cs.money>
2023-05-06 23:59:37 +0300

    test cases

---
+++ phonebook/index.js
@@ -0,0 +1,107 @@
+require('dotenv').config()
+
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const Person = require('./models/person')
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+
+    next(error)
+}
+
+const app = express()
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :response-time ms :body'))
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.get('/', (request, response, next) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response, next) => {
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+        .catch(err => next(err))
+})
+
+app.get('/info', (request, response, next) => {
+    Person.find({}).then(persons => {
+        const html_response = `<div> Phonebook has info ${persons.length} people <p> ${new Date()}</p></div>`
+        response.send(html_response)
+    }).catch(err => next(err))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+
+    Person.findByIdAndRemove(id).then(res => {
+        response.status(204).end()
+    }).catch(err => next(err))
+    // persons = persons.filter(p => p.id !== id)
+    // Person = Person.filter(p => p.id !== id)
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const {name, number} = request.body
+
+    const person = {
+        name, number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/persons/', (request, response, next) => {
+        const {name, number} = request.body
+
+        if (!name) {
+            return response.status(400).json({
+                error: 'name missing'
+            })
+        }
+
+        const person = new Person({name, number})
+
+        person
+            .save().then(savedPerson => {
+            response.json(savedPerson)
+        })
+            .catch(error => next(error))
+    }
+)
+
+app.get('/api/persons/:id', (request, response, next) => {
+
+    const id = request.params.id
+    Person.findById(id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(err => next(err))
+})
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ utils/list_helper.js
@@ -0,0 +1,45 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    return blogs.map(blog => blog.likes).reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    likes = blogs.map(b => b.likes)
+    maxLikes = Math.max(...likes)
+    console.log(maxLikes)
+
+    return blogs.find(b => b.likes === maxLikes)
+}
+
+const mostBlogs = (blogs) => {
+
+    const authorBlogs = _.map(_.countBy(blogs, "author"), (val, key) => ({author: key, blogs: val}))
+    return _.maxBy(authorBlogs, 'blogs')
+}
+
+const mostLikes = (blogs) => {
+    const likes = _(blogs)
+    .groupBy('author')
+    .map((objs, key) => ({
+        'author': key,
+        'likes': _.sumBy(objs, 'likes')
+    })).value()
+
+    return _.maxBy(likes, 'likes')
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
+}
